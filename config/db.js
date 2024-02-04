f4ad1de336790cb00a0ca33510f47aa1a754b26d
sarojpadhy123 <psarojkumar9@gmail.com>
2024-02-04 20:05:12 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require ('mongoose')
 async function connectDB(){
     try {
         // Database URL
-        return await mongoose.connect('mongodb+srv://psarojkumar9:TGtRbGyNwCrapvBo@cluster0.whyovyi.mongodb.net/')
-    } catch{
-        console.log('Database Connection Error')
+        return await mongoose.connect('mongodb+srv://psarojkumar9:sarojpass123@hospitalcluster.gikam2z.mongodb.net/')
+    } catch(error){
+        console.log(error);
     }
 }
 
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,9 @@ const jwt = require('jsonwebtoken')
 async function auth(req,res,next){
     try {
         const token = req.header('x-auth-token')
-        const decode = jwt.verify(token, process.env.JWT_SECRET) 
+      console.log(token)
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+     
         const staff = await Staffs.findOne({_id: decode._id})   
         req.staff = staff
         next()
